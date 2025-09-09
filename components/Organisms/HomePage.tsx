6c15c0e6b16ea65b233ee65873d6946bf2f445a7
mardin <tredingb25@gmail.com>
2025-09-09 11:22:05 +0700

    add: image in header

--- components/Organisms/HomePage.tsx
+++ components/Organisms/HomePage.tsx
@@ -1,20 +1,26 @@
 'use client';
 
+import Image from "next/image";
+
 const HomePage = () => {
   return (
     <>
       <div
         className='
-          w-full h-[200px] p-6 rounded-2xl flex items-center
+          w-full h-[200px] p-6 rounded-2xl flex items-center justify-between
           bg-white/20 dark:bg-gray-800/20
           backdrop-blur-xl
           border border-white/30 dark:border-gray-700/40
           shadow-lg
+          overflow-hidden
         '
       >
         <div className='w-max'>
           <h1 className='text-3xl font-bold capitalize drop-shadow-md'>Hi, Iam Mardin</h1>
           <p className='capitalize drop-shadow-sm'>I am a fullstack web developer</p>
+        </div>
+        <div className="w-max">
+          <Image alt="header" src="/header.png" width={250} height={250}/>
         </div>
       </div>
     </>
