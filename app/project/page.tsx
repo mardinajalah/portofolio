57bbc9aaa32bb36be11c497e55d5690c3915189d
mardin <tredingb25@gmail.com>
2025-10-17 13:05:48 +0700

    feat: add project simpan pinjam in page project

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -7,9 +7,14 @@ import { CardProject } from '@/components/Molecules/Card';
 const Project = () => {
   const [mounted, setMounted] = useState(false);
   const imageKasir: string[] = [];
+  const imageSimpanPinjam: string[] = [];
 
   for (let i = 1; i <= 22; i++) {
     imageKasir.push(`/assets/image-projects/toko-kasir/kasir${i}.png`);
+  }
+
+  for (let i = 1; i <= 13; i++) {
+    imageSimpanPinjam.push(`/assets/image-projects/simpan-pinjam/simpan${i}.png`);
   }
 
   useEffect(() => setMounted(true), []);
@@ -21,7 +26,8 @@ const Project = () => {
   return (
     <div className='w-full flex flex-wrap items-center sm:justify-center lg:justify-start gap-10'>
       {/* Card Project */}
-      <CardProject images={imageKasir} title='Dashboard Web Kasir' techStack='React TS, Tailwind CSS, Express JS, MySQL, Prisma' description='Web aplikasi kasir untuk usaha kecil dan menengah.' />
+      <CardProject images={imageKasir} title='Dashboard Web Kasir' techStack='React TS, DaisyUI, Tailwind CSS, Express JS, MySQL, Prisma' description='Web aplikasi kasir untuk usaha kecil dan menengah.' />
+      <CardProject images={imageSimpanPinjam} title='Dashboard Simpan Pinjam' techStack='React TS, DaisyUI, Tailwind CSS, Express JS, MySQL, Prisma' description='Web aplikasi simpan pinjam dan memiliki aplikasi mobilenya juga untuk usaha kecil dan menengah' />
     </div>
   );
 };
